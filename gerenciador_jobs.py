b73fe1804ee57a0e9791c1eb474a16b8b2cf75cb
Victor Wanderley <victor.wanderley@esig.com.br>
2023-10-28 22:24:49 -0300

    feat: Adicionando captura de exceção e modificando horário

--- gerenciador_jobs.py
+++ gerenciador_jobs.py
@@ -6,6 +6,9 @@ import time
 
 if __name__ == "__main__":
     logging.info("Iniciando Gerenciador Jobs")
-    job1 = schedule.every().hour.at(':20').do(org.organizar_pastas, diretorio='/home/victor/Downloads')
+    job1 = schedule.every().hour.at(':15').do(org.organizar_pastas, diretorio='/home/victor/Downloads')
     while True:
-        schedule.run_pending()
+        try:
+            schedule.run_pending()
+        except Exception as e:
+            logging.warning(e)
